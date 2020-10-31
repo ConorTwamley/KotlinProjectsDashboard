bdd5fe32a56db57386075a54921c6bf8d10fdb90
Conor Twamley <ctwamley@gmail.com>
2020-10-30 18:05:44 -0700

    Created a menu to house all of the examples i have done in Kotlin

--- app/src/main/java/com/conor/kotlinprojectsdashboard/MainActivity.kt
+++ app/src/main/java/com/conor/kotlinprojectsdashboard/MainActivity.kt
@@ -1,13 +1,35 @@
 package com.conor.kotlinprojectsdashboard
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.cardview.widget.CardView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val weatherTile = findViewById<CardView>(R.id.weather_tile)
+        val travelTile = findViewById<CardView>(R.id.travel_tile)
+        val educationTile = findViewById<CardView>(R.id.education_tile)
+        val medicalTile = findViewById<CardView>(R.id.medical_tile)
+
+        weatherTile.setOnClickListener{
+            val intent = Intent(this@MainActivity, WeatherActivity::class.java)
+            startActivity(intent)
+        }
+        travelTile.setOnClickListener{
+            Toast.makeText(applicationContext, "Travel", Toast.LENGTH_SHORT).show()
+        }
+        educationTile.setOnClickListener{
+            Toast.makeText(applicationContext, "Education", Toast.LENGTH_SHORT).show()
+        }
+        medicalTile.setOnClickListener{
+            Toast.makeText(applicationContext, "Medical", Toast.LENGTH_SHORT).show()
+        }
+
     }
-    
 
 }
