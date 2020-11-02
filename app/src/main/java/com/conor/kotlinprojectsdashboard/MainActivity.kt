63711a0f6f675064c73000ee92624d0b96798589
Conor Twamley <ctwamley@gmail.com>
2020-11-02 14:55:58 -0800

    Added Google Maps API and displaying of the users current location with a marker.

--- app/src/main/java/com/conor/kotlinprojectsdashboard/MainActivity.kt
+++ app/src/main/java/com/conor/kotlinprojectsdashboard/MainActivity.kt
@@ -12,7 +12,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val weatherTile = findViewById<CardView>(R.id.weather_tile)
-        val travelTile = findViewById<CardView>(R.id.travel_tile)
+        val locationTile = findViewById<CardView>(R.id.location_tile)
         val calculatorTile = findViewById<CardView>(R.id.calculator_tile)
         val medicalTile = findViewById<CardView>(R.id.medical_tile)
 
@@ -20,8 +20,9 @@ class MainActivity : AppCompatActivity() {
             val intentWeather = Intent(this@MainActivity, WeatherActivity::class.java)
             startActivity(intentWeather)
         }
-        travelTile.setOnClickListener{
-            Toast.makeText(applicationContext, "Travel", Toast.LENGTH_SHORT).show()
+        locationTile.setOnClickListener{
+            val intentLocation = Intent(this@MainActivity, MapsActivity::class.java)
+            startActivity(intentLocation)
         }
         calculatorTile.setOnClickListener{
             val intentCalculator = Intent(this@MainActivity, CalculatorActivity::class.java)
